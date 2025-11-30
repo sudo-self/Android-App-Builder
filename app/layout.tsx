fcbbd0f86c3f876d5e81282afe3cb67b5cf7ca33
sudo-self <sudo-self@proton.me>
2025-11-30 02:38:54 -0700

    Add Script import to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
+import Script from 'next/script'
+
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
